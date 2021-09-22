edf20409a72422cf189c103cd6c9d45ce46c77a1
ericjcili <ericjcili@gmai.com>
2021-09-22 19:59:04 -0400

    added user & post models and routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-// for when routes diretory is set up
-//const routes = require('./routes');
+
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// for when routes diretory is set up
-//app.use(routes);
+app.use(routes);
 
 // sync sequelize models to database & turn on server
 sequelize.sync({ force: false }).then(() => {
