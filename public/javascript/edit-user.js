4355b147b4e13235ea5956a2e6b41f5948e6e0bc
ericjcili <ericjcili@gmai.com>
2021-09-29 19:02:46 -0400

    minor changes

--- public/javascript/edit-user.js
+++ public/javascript/edit-user.js
@@ -14,13 +14,10 @@ let username = document.querySelector('input[name="user-name"]').value.trim();
     }
     const id = document.querySelector('input[name="user-id"]').value;
 
-    // Create a string with whichever updates were provided
     let userUpdate = '{' + [username, email, password].filter(value => value).join(',') + '}';
-    // Create the JSON parsed object
     userUpdate = JSON.parse(userUpdate)
 
 
-    // use the update route to update the post
     const response = await fetch(`/api/users/${id}`, {
         method: 'PUT',
         body: JSON.stringify(userUpdate),
@@ -28,10 +25,8 @@ let username = document.querySelector('input[name="user-name"]').value.trim();
           'Content-Type': 'application/json'
         }
       });
-    // if the edit action is successful, redirect to the dashboard page, otherwise display the error
     if (response.ok) {
         document.location.replace('/dashboard');
-        // otherwise, display the error
         } else {
         alert(response.statusText);
         }
